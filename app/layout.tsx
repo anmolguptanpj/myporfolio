664f11adf6e340465feffc7822ef2947d0afb1f1
anmolguptanpj <anmolguptanpj282@gmail.com>
2025-12-01 17:38:13 +0530

    title

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,9 @@ import "./globals.css";
 export default function RootLayout({children}: {children: React.ReactNode}) {
   return (
     <html lang="en">
+      <head>
+        <title>Anmol Gupta</title>
+      </head>
       <body className="bg-slate-900 w-full h-full overflow-y-auto">
           {children}
       </body>
